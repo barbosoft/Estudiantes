befd48d3cae30028ef1c41cf3b2e8ecef6b18a1e
Lluís Barbó López <107318977+barbosoft@users.noreply.github.com>
2022-10-17 19:28:13 +0200

    Create Firebase Inicializator
    - Create file to comunicate with Firebase
    - Changed and move files to best practice

---
+++ app/src/main/java/com/estudiantes/firebase/FirebaseInicializator.kt
@@ -0,0 +1,19 @@
+package com.estudiantes
+
+import android.content.Context
+import android.util.Log
+import com.google.firebase.FirebaseOptions
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.ktx.initialize
+
+private const val TAG = "FirebaseIncializator"
+
+fun inicializatorFirebase(context: Context) {
+    val options = FirebaseOptions.Builder() .setProjectId("estudiante-831bd") .setApplicationId("1:137210535639:android:da11f24cfe57d989ec5a92") .build()
+    try {
+        Firebase.initialize(context, options, "bd-firebase")
+    }
+    catch (e: Exception) {
+        Log.e(TAG, "FirebaseIncializator: ", e)
+    }
+}
