76bc44d55dbffeb2393ff3279e0e1600c6a3e554
Lluís Barbó López <107318977+barbosoft@users.noreply.github.com>
2022-10-19 21:04:30 +0200

    Created Adapter
    - Created Adapter
    - Set Adapter on Activity

--- app/src/main/java/com/estudiantes/ui/ListEstudiantesActivity.kt
+++ app/src/main/java/com/estudiantes/ui/ListEstudiantesActivity.kt
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.estudiantes.R
 import com.estudiantes.inicializatorFirebase
+import com.estudiantes.ui.adapter.ListEstudiantesAdapter
 import com.estudiantes.ui.dialog.dialogNewEstudiante
 import com.estudiantes.ui.viewModel.EstudiantesViewModel
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -16,6 +19,7 @@ class ListEstudiantesActivity : AppCompatActivity() {
         val provider = ViewModelProvider(this)
         provider.get(EstudiantesViewModel::class.java)
     }
+    private var adapter: RecyclerView.Adapter<ListEstudiantesAdapter.ViewHolder>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,7 +37,8 @@ class ListEstudiantesActivity : AppCompatActivity() {
                // )
            // )
 
-            dialogNewEstudiante(this, viewModel)
+            //dialogNewEstudiante(this, viewModel)
+            adapter!!.notifyDataSetChanged()
 
             // Comprovation in LOGcat
            /* var list = viewModel.getSearchEstudianteFirebase().value
@@ -43,6 +48,17 @@ class ListEstudiantesActivity : AppCompatActivity() {
                 Log.i("onCreateTest", "oncreate: Estudiante Nombre: $estudiante")
             }*/
         }
+
+        val recyclerView: RecyclerView = findViewById(R.id.list_estudiantes_activity_recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        adapter = ListEstudiantesAdapter(
+            this,
+            viewModel
+        )
+        recyclerView.adapter = adapter
+
+
 
 
     }
